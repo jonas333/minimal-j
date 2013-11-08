592b8d983a1b31057fbde35805044903a0b38d5a
Marco Musolf <marco.musolf@gmail.com>
2013-11-08 10:32:27 +0100

    initial check in

---
+++ Minimal-J_Lanterna/src/main/java/ch/openech/mj/lanterna/toolkit/LanternaCheckBox.java
@@ -0,0 +1,47 @@
+package ch.openech.mj.lanterna.toolkit;
+
+import ch.openech.mj.toolkit.ClientToolkit.InputComponentListener;
+
+import com.googlecode.lanterna.gui.component.CheckBox;
+import com.googlecode.lanterna.input.Key;
+
+public class LanternaCheckBox extends CheckBox implements ch.openech.mj.toolkit.CheckBox {
+
+	private final InputComponentListener changeListener;
+	
+	public LanternaCheckBox(InputComponentListener changeListener, String label) {
+		super(label, false);
+		this.changeListener = changeListener;
+	}
+
+	@Override
+	public void setChecked(boolean selected) {
+		if (isScrollable() != selected) {
+			select();
+		}
+	}
+
+	private void fireChangeEvent() {
+		changeListener.changed(LanternaCheckBox.this);
+	}
+	
+	@Override
+	public Result keyboardInteraction(Key key) {
+		Result result = super.keyboardInteraction(key);
+		if (result != Result.EVENT_NOT_HANDLED) {
+			fireChangeEvent();
+		}
+		return result;
+	}
+	
+	@Override
+	public void setEditable(boolean enabled) {
+		// not supported
+	}
+
+	@Override
+	public boolean isChecked() {
+		return isSelected();
+	}
+
+}
