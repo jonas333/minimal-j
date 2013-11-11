a3373139587de9a28f6d74f605c6412d9405050b
Marco Musolf <marco.musolf@gmail.com>
2013-11-11 16:25:33 +0100

    removed unused package

--- Minimal-J_Android/src/ch/openech/mj/android/toolkit/AndroidDialog.java
+++ Minimal-J_Android/src/ch/openech/mj/android/toolkit/AndroidDialog.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
 import android.os.Bundle;
 import android.view.View;
 import ch.openech.mj.toolkit.IComponent;
@@ -14,7 +16,13 @@ public class AndroidDialog extends DialogFragment implements IDialog {
 	private String title;
 	private IComponent content;
 	private FragmentManager fragmentManager;
+	private CloseListener listener;
 
+	
+	public AndroidDialog() {
+		super();
+	}
+	
 	public void setFragmentManager(FragmentManager fragmentManager) {
 		this.fragmentManager = fragmentManager;
 	}
@@ -26,6 +34,17 @@ public class AndroidDialog extends DialogFragment implements IDialog {
 		builder.setView((View) content);
 		builder.setPositiveButton("OK", null);
 		builder.setNegativeButton("Abbrechen", null);
+		Dialog dlg = builder.create();
+		dlg.setOnDismissListener(new OnDismissListener() {
+			
+			@Override
+			public void onDismiss(DialogInterface dialog) {
+				if (listener != null)
+				{
+					listener.close();
+				}
+			}
+		});
 		return builder.create();
 		
 	}
@@ -42,8 +61,7 @@ public class AndroidDialog extends DialogFragment implements IDialog {
 
 	@Override
 	public void setCloseListener(CloseListener listener) {
-		// TODO Auto-generated method stub
-
+		this.listener = listener;
 	}
 	
 	public void setTitle(String title) {
