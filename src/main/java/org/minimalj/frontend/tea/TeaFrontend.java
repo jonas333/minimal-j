0b6663c832c7f362adf8b26ecea9011732a5cee1
bruno <bruno.eberhard@pop.ch>
2015-02-01 14:37:40 +0100

    Small try with TeaVM. Doesn't work because of LocalDates

---
+++ src/main/java/org/minimalj/frontend/tea/TeaFrontend.java
@@ -0,0 +1,13 @@
+package org.minimalj.frontend.tea;
+
+import org.minimalj.util.FieldUtils;
+
+public class TeaFrontend {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		FieldUtils.isAllowedPrimitive(Double.class);
+	}
+
+}
