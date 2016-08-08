7b66351b8e351cfb5471b64d193f785ca8ca3548
bruno <bruno.eberhard@pop.ch>
2016-08-08 14:40:47 +0200

    NotEmpty annotation

--- example/Minimal-J_Currencies/src/org/minimalj/example/currencies/model/CurrencyCalculation2.java
+++ example/Minimal-J_Currencies/src/org/minimalj/example/currencies/model/CurrencyCalculation2.java
@@ -3,12 +3,15 @@ package org.minimalj.example.currencies.model;
 import java.math.BigDecimal;
 
 import org.minimalj.model.Keys;
+import org.minimalj.model.annotation.NotEmpty;
 
 public class CurrencyCalculation2 {
 
 	public static final CurrencyCalculation2 $ = Keys.of(CurrencyCalculation2.class);
 	
+	@NotEmpty
 	public Currency from, to;
 	
+	@NotEmpty
 	public BigDecimal fromValue;
 }
