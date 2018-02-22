59f8621bde73d94ceb84e70af5ad25c18366fa4e
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-02-22 17:46:28 +0100

    Working version
    will go on other branch to consolidate

--- src/main/java/org/minimalj/frontend/impl/cheerpj/Cheerpj.java
+++ src/main/java/org/minimalj/frontend/impl/cheerpj/Cheerpj.java
@@ -5,19 +5,14 @@ import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.impl.json.JsonFrontend;
 import org.minimalj.frontend.impl.json.JsonSessionManager;
 import org.minimalj.model.test.ModelTest;
-import org.minimalj.util.StringUtils;
 
 public class Cheerpj {
 
 	private static JsonSessionManager sessionManager = new JsonSessionManager();
 	
-	public static native void sendMessage(String json);
-	
-	public static void receiveMessage(String json) {
+	public static String receiveMessage(String json) {
 		String result = sessionManager.handle(json);
-		if (StringUtils.isEmpty(result)) {
-			sendMessage(json);
-		}
+		return result;
 	}
 	
 	public static void start(Application application) {
