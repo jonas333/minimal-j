5c7af2acd6179b81448f081311f2ef6b45c5437d
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-02-22 17:47:07 +0100

    Sandbox Class

---
+++ example/002_HelloWorld/src/org/minimalj/example/helloworld/Pong.java
@@ -0,0 +1,12 @@
+package org.minimalj.example.helloworld;
+
+public class Pong {
+
+	public static String receiveMessage(String input) {
+		return "Hello " + input;
+	}
+	
+	public static void main(String[] args) {
+		// do nothing
+	}
+}
