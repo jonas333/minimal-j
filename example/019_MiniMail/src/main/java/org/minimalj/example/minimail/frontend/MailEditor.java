dd8eaba050ceb04eecb3966d448d07d692bbc7e1
Marco Musolf <marcomusolf@hotmail.com>
2018-11-06 15:42:07 +0100

    mail sample extended

--- example/019_MiniMail/src/main/java/org/minimalj/example/minimail/frontend/MailEditor.java
+++ example/019_MiniMail/src/main/java/org/minimalj/example/minimail/frontend/MailEditor.java
@@ -1,12 +1,21 @@
 package org.minimalj.example.minimail.frontend;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.mail.MessagingException;
+
 import org.minimalj.backend.Backend;
+import org.minimalj.example.minimail.backend.MailService;
 import org.minimalj.example.minimail.model.Mail;
 import org.minimalj.frontend.editor.Editor.SimpleEditor;
 import org.minimalj.frontend.form.Form;
 import org.minimalj.frontend.form.element.StringFormElement;
+import org.minimalj.model.properties.Properties;
 
 public class MailEditor extends SimpleEditor<Mail> {
+	
+	private static final Logger LOGGER = Logger.getLogger(Properties.class.getName());
 
 	private final class MailForm extends Form<Mail> {
 		public MailForm() {
@@ -35,6 +44,11 @@ public class MailEditor extends SimpleEditor<Mail> {
 
 	@Override
 	protected Mail save(Mail object) {
+		try {
+			MailService.sendMail(object);
+		} catch (MessagingException e) {
+			LOGGER.log(Level.SEVERE, String.format("unable to send mail to recipient %s", object.from.address), e);
+		}
 		return Backend.save(object);
 	}
 
